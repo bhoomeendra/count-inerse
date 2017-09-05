e84222c0161704f3124e328aa1d66fa00c8d16c5
Bhoomeendra Singh Sisodiya <bhoomeendra.pro@gmail.com>
2017-09-05 07:43:04 +0530

    septchal

--- tough.cpp
+++ tough.cpp
@@ -1,87 +1,135 @@
 #include<bits/stdc++.h>
 using namespace std;
-int count(int *a,int start,int mid,int end)
+
+struct matrix
 {
-	int ls=start;
-	int rs=mid+1;
-	int b[end-start+1];
-	int sum=0;
-	int countt=0;
-	int i=0;
-	while((ls<mid+1)&&(rs<end+1))
-	{
-		if(a[ls]>a[rs])
-		{
-			b[i]=a[ls];
-			++ls;++i;
-			++countt;
-		}
-		else
-		{
-			b[i]=a[rs];
-			++rs;++i;
-			sum=sum+countt;
-		}
-	}
-	if(ls>=mid+1)
-	{
-		while(rs<end+1)
-		{
-		b[i]=a[rs];
-			++rs;++i;
-			sum=sum+countt;	
-		}
-	}
-	else
-	{
-while(ls<mid+1)
+	int row,col,val;
+};
+int comp(const void *a,const void *b)
 {
-				b[i]=a[ls];
-			++ls;++i;
-			++countt;
-}
-	}
-	i=0;
-	ls=start;
-	while(i<end-start+1)
-	{
-		a[ls]=b[i];
-		++ls;
-		++i;
-	}
-	cout<<sum<<end;
-	return sum;
-}
-int inver(int *a,int start,int end)
-{
-	if(start==end)
-	{
-		return 0;
-	}
-	int mid=(start+end)/2;
-	int i=inver(a,start,mid);
-	int j=inver(a,mid+1,end);
-	int k=count(a,start,mid,end);
-	return (i+j+k);
+	matrix *af=(matrix *)a;
+	matrix *bf=(matrix *)b;
+		if(af->row!=bf->row)
+		{
+			return(af->row>bf->row);
+		}
+		
+			return(af->col>bf->col);
 }
 int main()
 {
-	int size;
-	cin>>size;
-	int a[size];
-	int i=0;
-	while(i<size)
+	int t;
+	//cout<<1<<endl;
+	cin>>t;
+	while(t--)
 	{
-		cin>>a[i];
-		++i;
-	}
-	int f=inver(a,0,size);
-	cout<<f<<endl;
+		int n,q;
+		cin>>n>>q;
+		int i=0;
+		int b[n][n];
 		i=0;
-		while(i<size)
-	{
-		cout<<a[i]<<endl;
+		while(i<n)
+		{
+			int j=0;
+			while(j<n)
+			{
+				b[i][j]=-1;
+				++j;
+			}
 		++i;
+		}
+		matrix ar[q];
+		int row,col,val;
+		bool found=false;
+		i=0;
+		int j=0;
+		while(i<q)
+		{
+			//cout<<2<<endl;
+			cin>>row>>col>>val;
+			if(row<col)
+			{
+			b[row-1][col-1]=val;
+			ar[j].row=row;
+			ar[j].col=col;
+			ar[j].val=val;
+			++j;
+			}
+			else if(row==col)
+			{
+				if(val!=0)
+				{found=true;break;}
+			b[row-1][col-1]=val;
+			ar[j].row=row;
+			ar[j].col=col;
+			ar[j].val=val;	
+			++j;
+			}
+			else if(row>col)
+			{
+				if(b[col-1][row-1]==-1)
+				{b[col-1][row-1]=val;
+					ar[j].row=col;
+					ar[j].col=row;
+					ar[j].val=val;
+					++j;
+				}
+				else if(b[col-1][row-1]!=val)
+				{found=true;}
+			}
+			++i;
+		}
+		if(found)
+		{
+			cout<<"no"<<endl;
+			continue;
+		}
+		int size_ar=j;
+		qsort(ar,size_ar,sizeof(matrix),comp);
+		i=0;
+		/*while(i<size_ar)
+		{
+			cout<<ar[i].row<<","<<ar[i].col<<endl;
+			++i;
+		}*/
+		i=0;
+		while(i<size_ar)
+		{
+			int j=i+1;
+			while(j<size_ar)
+			{
+				if(ar[i].row==ar[j].row)
+				{
+					if(b[ar[i].col-1][ar[j].col-1]!=-1)
+					{
+						int check=ar[i].val+ar[j].val+b[ar[i].col-1][ar[j].col-1];
+						if(check==3||check==1)
+						{
+							found=true;
+							break;
+						}
+						
+					}		
+				}
+				else
+				{break;}
+				++j;		
+			}
+		++i;
+		if(found)
+		{
+			break;
+				}		
+		}
+		if(found)
+		{
+			cout<<"no"<<endl;
+		}
+		else
+		{
+			cout<<"yes"<<endl;
+				}		
 	}
+	
 	return 0;
 }
